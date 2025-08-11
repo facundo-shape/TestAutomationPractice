5d79ab0eef9ee3ca4819edde3bc1c40901d5f358
facundo-shape <facundo@shapegames.com>
2025-08-11 09:36:14 +0200

    First commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,67 @@
+package org.example
+
+fun main() {
+
+}
+
+fun assignmentOne() {
+    // Create a variable called daysInWeek with the value 7. Don't write the type — let Kotlin figure it out.
+    val daysInWeek = 7
+}
+
+fun assignmentTwo() {
+    // Make a variable called userScore with a starting value of 0. Add 10 to it and print the result.
+    var userScore = 0
+    userScore += 10
+    println(userScore)
+}
+
+fun assignmentThree() {
+    // Create width and height variables. Find the area of a rectangle.
+    val width: Int = 3
+    val height: Int = 5
+    rectangleArea = width * heigh
+}
+
+fun assignmentFour() {
+    // Make a variable pi with type Double and value 3.14159.
+    val pi: Double = 3.114159
+}
+
+fun assignmentFive() {
+    // Add a comment explaining a variable that stores a user's name.
+    // Variable that stores the users name in a string format
+}
+
+fun assignmentSix() {
+    // Check if the number 25 is even, and store the result in isEven.
+    var isEven: Boolean
+    var numberToEvaluate = 25
+    isEven = ((numberToEvaluate % 2) == 0)
+}
+
+fun assignmentSeven() {
+    // Combine firstName and lastName into one variable called fullName.
+    val firstName = "Facundo"
+    val lastName = "Villarroel"
+    val fullName = "$firstName $lastName"
+}
+
+fun assignmentEight() {
+    /*
+    Why does this code not compile? Fix it and explain in a comment.
+
+        // Original code with error
+        fun main() {
+            val balance = 100
+            balance = 120 // Error here
+            println(balance)
+        }
+     */
+
+    // Trying to reassign value to a constant
+}
+
+fun assignmentNine() {
+
+}
