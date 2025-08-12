a86dc16e085d08a6e478c46b92835682b47bf694
facundo-shape <facundo@shapegames.com>
2025-08-12 09:40:49 +0200

    Add assignments 9 to 15

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,16 +16,18 @@ fun assignmentTwo() {
     println(userScore)
 }
 
-fun assignmentThree() {
+fun assignmentThree(): Int {
     // Create width and height variables. Find the area of a rectangle.
     val width: Int = 3
     val height: Int = 5
-    rectangleArea = width * heigh
+    val rectangleArea = width * height
+    return rectangleArea
 }
 
-fun assignmentFour() {
+fun assignmentFour(): Double {
     // Make a variable pi with type Double and value 3.14159.
     val pi: Double = 3.114159
+    return pi
 }
 
 fun assignmentFive() {
@@ -33,18 +35,20 @@ fun assignmentFive() {
     // Variable that stores the users name in a string format
 }
 
-fun assignmentSix() {
+fun assignmentSix(): Boolean {
     // Check if the number 25 is even, and store the result in isEven.
     var isEven: Boolean
     var numberToEvaluate = 25
     isEven = ((numberToEvaluate % 2) == 0)
+    return isEven
 }
 
-fun assignmentSeven() {
+fun assignmentSeven(): String {
     // Combine firstName and lastName into one variable called fullName.
     val firstName = "Facundo"
     val lastName = "Villarroel"
     val fullName = "$firstName $lastName"
+    return fullName
 }
 
 fun assignmentEight() {
@@ -60,8 +64,53 @@ fun assignmentEight() {
      */
 
     // Trying to reassign value to a constant
+    val balance = 100
+    println(balance)
 }
 
-fun assignmentNine() {
+fun assignmentNine(): Boolean {
+    // Check if myAge is enough to meet requiredAge.
+    val requiredAge = 30
+    var myAge = 37
+    return myAge>requiredAge
+}
 
+fun assignmentTen() {
+    // Describe the logic of code that calculates a price with a discount.
+
+    /*
+    To calculate a price discount this function takes as a parameter the price and the discount percentage,
+    where it applies the discount my means of diving the percentage parameter by 100 and multiplying the result
+    by the price of the item, returning the item price as Double
+     */
+}
+
+fun assignmentEleven() {
+    // Store whether a user is active. Pick the best type and assign true.
+    var isUserActive: Boolean = true
+}
+
+fun assignmentTwelve() {
+    // Print a message about the temperature. If it's over 20, say "It is warm", otherwise say "It is cold".
+    var temperature = 22
+    val message = "$temperature is ${if(temperature>20) "is hot" else "is cold"}"
+    println(message)
+}
+
+fun assignmentThirteen() {
+    // Count apples in baskets and some extra apples.
+}
+
+fun assignmentFourteen(): Boolean {
+    // See if the door can be opened: you need both a key and the password.
+    val doorKey = false
+    val doorPassword = true
+    val accessGranted = doorKey && doorPassword
+    return accessGranted
+}
+
+fun assignmentFifteen() {
+    // Increase messageCount by 1 and print the result.
+    var messageCount = 0
+    println(messageCount++)
 }
